15e917ce7452b2a2bfb5dd00bf0dcaf03351f295
dongdong <zisehaiyang04@163.com>
2019-08-21 20:31:56 +0800

    完善提交报价信息20190821

--- test_clue.py
+++ test_clue.py
@@ -1,53 +1,48 @@
 import unittest
-from send_clue import Send_Clue, Trademark
+from send_clue import Send_Clue
 
 goods_list_info = Send_Clue().test_list_page()
-goods_detail_info = Trademark().trademark_list()
-
-"""
-{
-            "goods_name": goods_name,
-            "goods_type": goods_type,
-            "goods_suit": goods_suit,
-            "goods_deadline": goods_deadline,
-            "goods_price": goods_price
-        }
-"""
-"""
-{
-            "goods_name": self.goods_name,
-            "goods_type": self.goods_type,
-            "goods_price": float(self.goods_price),
-            "goods_detail_name": self.goods_detail_name,
-            "goods_detail_type": self.goods_detail_type,
-            "goods_detail_price": float(self.goods_detail_price)
-        }
-"""
 
 
 class TestListClue(unittest.TestCase):
     def setUp(self):
         self.goods_list_info = goods_list_info
-        self.goods_detail_info = goods_detail_info
+        # self.goods_detail_info = goods_detail_info
 
-    def test_name_page_detail(self):
-        """测试列表页提交报价中商标名称和详情页中是否一致"""
-        self.assertEqual(self.goods_list_info.get("goods_name"),self.goods_detail_info.get("goods_name"))
+    def test_name_frame_list(self):
+        """测试列表页提交报价中商标名称和列表页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_name"), self.goods_list_info.get("list_goods_name"))
 
-    def test_type_page_detail(self):
-        """测试列表页提交报价中商标名称和详情页中是否一致"""
-        self.assertEqual(self.goods_list_info.get("goods_type"),self.goods_detail_info.get("goods_type"))
+    def test_type_frame_list(self):
+        """测试列表页提交报价中商标名称和列表页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_type"), self.goods_list_info.get("list_goods_type"))
 
-    def test_price_page_detail(self):
-        """测试列表页提交报价中商标名称和详情页中是否一致"""
-        self.assertEqual(self.goods_list_info.get("goods_price"),self.goods_detail_info.get("goods_price"))
+    def test_price_frame_list(self):
+        """测试列表页提交报价中商标名称和列表页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_price"), self.goods_list_info.get("list_goods_price"))
 
-    def test_deadline_page_detail(self):
-        """测试列表页提交报价中商标名称和详情页中是否一致"""
-        self.assertEqual(self.goods_list_info.get("goods_deadline"),self.goods_detail_info.get("goods_deadline"))
+    def test_deadline_frame_list(self):
+        """测试列表页提交报价中有效期限和列表页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_deadline"),
+                         self.goods_list_info.get("goods_detail_deadline"))
 
     def test_send_result_list(self):
-        """测试是否发送成功"""
+        """测试提交报价是否发送成功"""
         self.assertEqual(self.goods_list_info.get("send_result"), True)
 
+    def test_name_frame_detail(self):
+        """测试列表页提交报价中商标名称和详情页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_name"), self.goods_list_info.get("goods_detail_name"))
 
+    def test_type_frame_detail(self):
+        """测试列表页提交报价中商标名称和详情页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_type"), self.goods_list_info.get("goods_detail_type"))
+
+    def test_price_frame_detail(self):
+        """测试列表页提交报价中商标名称和详情页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_price"), self.goods_list_info.get("goods_detail_price"))
+
+    def test_deadline_frame_detail(self):
+        """测试列表页提交报价中有效期限和详情页中是否一致"""
+        self.assertEqual(self.goods_list_info.get("frame_goods_deadline"),
+                         self.goods_list_info.get("goods_detail_deadline"))
