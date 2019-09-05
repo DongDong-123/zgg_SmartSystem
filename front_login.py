fd173f83ead06af4f35ab8845588371b6fb57661
dongdong <zisehaiyang04@163.com>
2019-09-05 20:41:47 +0800

    修改登陆url，绕开首页弹窗干扰20190905

--- front_login.py
+++ front_login.py
@@ -4,23 +4,29 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from readConfig import ReadConfig
-
+import time
 
 def front_login(username, password):
-    from selenium.webdriver.chrome.options import Options
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    # from selenium.webdriver.chrome.options import Options
+    # chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    # driver = webdriver.Chrome(chrome_options=chrome_options)
 
-    # driver = webdriver.Chrome()
+    driver = webdriver.Chrome()
 
     driver.maximize_window()
-    driver.get(ReadConfig().get_root_url())
+    # driver.get(ReadConfig().get_root_url())
+    # windows = driver.window_handles
+    # print(windows)
     # 等待页面加载完成
-    locator = (By.LINK_TEXT, u'立即登录')
-    WebDriverWait(driver, 30, 0.5).until(EC.element_to_be_clickable(locator))
-    # 进入登录页面
-    driver.find_element_by_link_text(u"立即登录").click()
+    # time.sleep(1)
+    driver.get(ReadConfig().login_page())
+
+    # locator = (By.LINK_TEXT, u'立即登录')
+    # WebDriverWait(driver, 30, 0.5).until(EC.element_to_be_clickable(locator))
+    # # 进入登录页面
+    # driver.find_element_by_link_text(u"立即登录").click()
+
     locator = (By.LINK_TEXT, '密码登录')
     WebDriverWait(driver, 10, 0.5).until(EC.presence_of_element_located(locator))
     driver.find_element_by_link_text(u'密码登录').click()
@@ -28,6 +34,7 @@ def front_login(username, password):
     driver.find_element_by_id('tb_user').send_keys(username)
     driver.find_element_by_id('tb_password').send_keys(password)
     driver.find_element_by_id('login1').click()
+    # 登录成功
     sleep(4)
     return driver
     # driver.quit()
